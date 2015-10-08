e250b0bad6ee81e22d011166b72167ec34807c6b
arshdeep <arshsingh_sc@yahoo.com>
2015-10-08 11:21:36 -0700

    Started adding code to the Tempterature activity file.

--- Documents/AndroidProjs/Pweather/app/src/main/java/io/arsh/pweather/TempActivity.java
+++ Documents/AndroidProjs/Pweather/app/src/main/java/io/arsh/pweather/TempActivity.java
@@ -6,20 +6,57 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 
 public class TempActivity extends AppCompatActivity {
+
+    @Bind(R.id.timeLabel) TextView myTimeLabel;
+    @Bind(R.id.temperatureLabel) TextView myTemperatureLabel;
+    @Bind(R.id.humidityValue) TextView myHumidityValue;
+    @Bind(R.id.precipValue) TextView myPrecipValue;
+    @Bind(R.id.summaryLabel) TextView mySummaryLabel;
+    @Bind(R.id.iconImageView) ImageView myIconImageView;
+    @Bind(R.id.refreshImageView) ImageView myRefreshImageView;
+    @Bind(R.id.progressBar) ProgressBar myProgressBar;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp);
+        ButterKnife.bind(this); //wrote this so that butterknife would run
+
+        myProgressBar.setVisibility(View.INVISIBLE); //originally not seen.
+
+        final double latitude = TheLocUtil.getLatitude();
+        final double longitude = TheLocUtil.getLongitude();
 
 
 
+        myRefreshImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                //check if data is connected, if not then change summary label to say "connect to net"
+
+                getForecast(latitude, longitude);
+            }
+        });
 
 
         //check if data is connected, if not then change summary label to say "connect to net"
+
+        getForecast(latitude, longitude);
+
+
 
         //summary label should change to white after data is loaded
 

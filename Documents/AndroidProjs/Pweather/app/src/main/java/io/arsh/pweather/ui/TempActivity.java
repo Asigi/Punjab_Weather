688210f77af9269b2aba4f730a3583343a8577e2
arshdeep <arshsingh_sc@yahoo.com>
2015-10-09 21:26:10 -0700

    Figuring out how to deal with Open API.

--- Documents/AndroidProjs/Pweather/app/src/main/java/io/arsh/pweather/ui/TempActivity.java
+++ Documents/AndroidProjs/Pweather/app/src/main/java/io/arsh/pweather/ui/TempActivity.java
@@ -47,7 +47,8 @@ public class TempActivity extends AppCompatActivity {
     @Bind(R.id.progressBar) ProgressBar myProgressBar;
 
     private static final String TAG = TempActivity.class.getSimpleName();
-    private Forecast myForcast;
+    private Forecast myOpenForecast; //OPEN
+    private Forecast myDarkForecast; //DARK SKY
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,19 +79,21 @@ public class TempActivity extends AppCompatActivity {
     }
 
 
-    private void getForecast(double theLat, double theLong) {
-        //String apiKey = "c6b2ddbf6c41031ec01ee83c14dca99a"; open weather
-        String apiKey = "18131678eb944e61639caac84cb622b6"; //forecast.io
+    private void getForecast(final double theLat, final double theLong) {
+        String apiKeyW = "c6b2ddbf6c41031ec01ee83c14dca99a"; //open weather, use for current temperature
+        String forecastURLW = "http://api.openweathermap.org/data/2.5/weather?lat=" + theLat
+                + "&lon=" + theLong + "APPID={" + apiKeyW + "}";
 
-        String forecastURL = "https://api.forecast.io/forecast/" + apiKey +
-                "/" + theLat + "," + theLong;
+        //http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID={APIKEY}
+        //api.openweathermap.org/data/2.5/weather?lat=35&lon=139
+        //api.openweathermap.org/data/2.5/find?q=London&units=metric
 
         if (TheNetUtil.isNetworkAvailable(TempActivity.this)) {
             toggleRefresh();
 
             OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder()
-                    .url(forecastURL)
+                    .url(forecastURLW)
                     .build();
             Call call = client.newCall(request);
             call.enqueue(new Callback() {
@@ -118,7 +121,7 @@ public class TempActivity extends AppCompatActivity {
                         String jsonData = response.body().string();
                         Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
-                            myForcast = parseForecastDetails(jsonData);
+                            myOpenForecast = parseOpenForecastDetails(jsonData);
                             //myCurWeather = getCurrentDetails(jsonData);
                             runOnUiThread(new Runnable() { //this code will cause whatever is in the inner run method to run on the main thread.
                                 @Override
@@ -134,6 +137,47 @@ public class TempActivity extends AppCompatActivity {
                     } catch (JSONException e) {
                         Log.e(TAG, "Exception caught: ", e);
                     }
+
+
+
+                    //NOW DO THE LOAD FROM DARK SKYS
+
+                    String apiKey = "18131678eb944e61639caac84cb622b6"; //forecast.io, use for hourly/7day
+                    String forecastURL = "https://api.forecast.io/forecast/" + apiKey +
+                            "/" + theLat + "," + theLong;
+
+                    OkHttpClient client2 = new OkHttpClient();
+                    Request request2 = new Request.Builder()
+                            .url(forecastURL)
+                            .build();
+                    Call call2 = client2.newCall(request2);
+                    call2.enqueue(new Callback() {
+                        @Override
+                        public void onFailure(Request request2, IOException e) {
+
+                            alertUserAboutError();
+                        }
+
+                        @Override
+                        public void onResponse(Response response) throws IOException {
+
+                            try {
+                                String jsonData = response.body().string();
+                                Log.v(TAG, jsonData);
+                                if (response.isSuccessful()) {
+                                    myDarkForecast = parseForecastDetails(jsonData); //TODO This line must be called BEFORE the line that calls parseOpenForecastDetails method which is about 30 lines up from here!!!
+
+                                } else {
+                                    alertUserAboutError();
+                                }
+                            } catch (IOException e) {
+                                Log.e(TAG, "Exception caught: ", e);
+                            } catch (JSONException e) {
+                                Log.e(TAG, "Exception caught: ", e);
+                            }
+                        }
+                    });
+
                 }
             });
 
@@ -145,14 +189,14 @@ public class TempActivity extends AppCompatActivity {
     @OnClick(R.id.dailyButton)
     public void startDailyActivity(View view) {
         Intent intent = new Intent(this, DailyForecastActivity.class);
-        intent.putExtra(MainActivity.DAILY_FORECAST, myForcast.getDailyForecast());
+        intent.putExtra(MainActivity.DAILY_FORECAST, myDarkForecast.getDailyForecast());
         startActivity(intent);
     }
 
     @OnClick(R.id.hourlyButton)
     public void startHourlyActivity(View view) {
         Intent intent = new Intent(this, HourlyForecastActivity.class);
-        intent.putExtra(MainActivity.HOURLY_FORECAST, myForcast.getHourlyForecast());
+        intent.putExtra(MainActivity.HOURLY_FORECAST, myDarkForecast.getHourlyForecast());
         startActivity(intent);
     }
 
@@ -167,7 +211,7 @@ public class TempActivity extends AppCompatActivity {
     }
 
     private void updateDisplay() {
-        Current current = myForcast.getCurrent();
+        Current current = myOpenForecast.getCurrent();
         myTemperatureLabel.setText(current.getMyTemperature() + "");
         myTimeLabel.setText("At " + current.getFormattedTime() + " it will be");
         myHumidityValue.setText(current.getMyHumidity() + "");
@@ -186,6 +230,61 @@ public class TempActivity extends AppCompatActivity {
         forecast.setDailyForecast(getDailyForecast(jsonData));
 
         return forecast;
+    }
+
+    private Forecast parseOpenForecastDetails(String jsonData) throws JSONException {
+        Forecast forecast = new Forecast();
+
+        forecast.setCurrent(getCurrentDetails2(jsonData));
+
+        return forecast;
+    }
+
+    Long findTime; //this will be retreived from the Dark json, and used for multiple things.
+
+    private Current getCurrentDetails(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+        String timezone = forecast.getString("timezone");
+        Log.i(TAG, "From JSON: " + timezone);
+
+        JSONObject currently = forecast.getJSONObject("currently"); //get currently JSON from within forecast JSON
+
+        Current current = new Current();
+        current.setMyHumidity(currently.getDouble("humidity"));
+        findTime = currently.getLong("time");
+        current.setMyTime(findTime);
+        current.setMyIcon(currently.getString("icon"));
+        current.setMyPrecipChance(currently.getDouble("precipProbability"));
+        current.setMySummary(currently.getString("summary"));
+        current.setMyTemperature(currently.getDouble("temperature"));
+        current.setMyTimeZone(timezone);
+
+        Log.d(TAG, current.getFormattedTime());
+
+        return current;
+    }
+
+    private Current getCurrentDetails2(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+        JSONObject Sys = forecast.getJSONObject("sys");
+        String sunrise = Sys.getString("sunrise");
+        String sunset = Sys.getString("sunset"); //TODO, maybe use these
+
+        JSONObject main = forecast.getJSONObject("main");
+
+        Current current = new Current();
+
+        current.setMyHumidity(main.getDouble("humidity"));
+        current.setMyTemperature(main.getDouble("temp"));
+        current.setMyTime(findTime);
+
+        JSONObject weath = forecast.getJSONObject("weather");
+        //get summary(description) from weath
+        //get icon info from weath and set icon of current
+
+        //timezone is always Delhi time
+
+        return current;
     }
 
 
@@ -240,26 +339,7 @@ public class TempActivity extends AppCompatActivity {
     }
 
 
-    private Current getCurrentDetails(String jsonData) throws JSONException{
-        JSONObject forecast = new JSONObject(jsonData);
-        String timezone = forecast.getString("timezone");
-        Log.i(TAG, "From JSON: " + timezone);
 
-        JSONObject currently = forecast.getJSONObject("currently");
-
-        Current current = new Current();
-        current.setMyHumidity(currently.getDouble("humidity"));
-        current.setMyTime(currently.getLong("time"));
-        current.setMyIcon(currently.getString("icon"));
-        current.setMyPrecipChance(currently.getDouble("precipProbability"));
-        current.setMySummary(currently.getString("summary"));
-        current.setMyTemperature(currently.getDouble("temperature"));
-        current.setMyTimeZone(timezone);
-
-        Log.d(TAG, current.getFormattedTime());
-
-        return current;
-    }
 
 
     private void alertUserAboutError() {

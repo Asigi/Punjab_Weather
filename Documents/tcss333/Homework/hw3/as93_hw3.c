b2969ecb9816fd6ce592463f2bdab3da7aea8262
arshdeep <arshsingh_sc@yahoo.com>
2015-11-11 10:32:26 -0800

    initial

---
+++ Documents/tcss333/Homework/hw3/as93_hw3.c
@@ -0,0 +1,38 @@
+
+
+/*Arshdeep Singh
+    C Programming
+    Homework 3
+
+    Comment explaining my logic:
+ 
+ 
+ 
+*/
+
+
+
+
+#include <stdio.h>
+
+
+//argc is the number of arguments passed in
+//argv contains the arguments
+int main( int argc, char *argv[] ) {
+    
+    
+    if ( argc != ) {
+        printf("Wrong number of arguments");
+    } else {
+        
+        //do the rest of the work here.
+        
+        
+        
+        
+        
+        
+    
+    
+    }
+}
